5b87955fd52c3c6b560aac0b009c0b67c2b3d9ca
MacUser <macuser@MacUsers-Air.lan1>
2019-01-17 17:22:11 -0500

    first commit

---
+++ src/isabella_java_hw2/Problem4.java
@@ -0,0 +1,31 @@
+package isabella_java_hw2;
+
+public class Problem4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* Circle 1 coordinates */
+		Scanner circle_1_coords_input = new Scanner(System.in);
+		System.out.println("What are circle 1's center coordinates?");
+		String circle_1_coords = circle_1_coords_input.nextLine();
+		String[] circle_1_coords_split = circle_1_coords.split("\\s");
+		String circle_1_coord_1 = circle_1_coords_split[0];
+		String circle_1_coord_2 =circle_1_coords_split[1];
+		float circle_1_coord_1_int = Integer.parseInt(circle_1_coord_1);
+		float circle_1_coord_2_int = Integer.parseInt(circle_1_coord_2);	
+		
+		/* Circle 2 coordinates */
+		Scanner circle_2_coords_input = new Scanner(System.in);
+		System.out.println("What are circle 2's center coordinates?");
+		String circle_2_coords = circle_2_coords_input.nextLine();
+		String[] circle_2_coords_split = circle_2_coords.split("\\s");
+		String circle_2_coord_1 = circle_2_coords_split[0];
+		String circle_2_coord_2 =circle_2_coords_split[1];
+		float circle_2_coord_1_int = Integer.parseInt(circle_2_coord_1);
+		float circle_2_coord_2_int = Integer.parseInt(circle_2_coord_2); 
+		
+
+	}
+
+}
