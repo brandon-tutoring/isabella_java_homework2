32b21855663ab542ffe05f99c86f7564e74dbb6f
MacUser <macuser@MacUsers-Air.lan1>
2019-01-20 08:22:01 -0500

    test commit

--- src/isabella_java_hw2/Problem3.java
+++ src/isabella_java_hw2/Problem3.java
@@ -1,69 +1,79 @@
 package isabella_java_hw2;
 
+import java.nio.charset.Charset;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Problem3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner Input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		System.out.println("let's play a game of odds and evens!");
 		System.out.println("What kind of player do you want the computer to be?(h or c)");
 		System.out.println("(H)onest Olivia");
-		System.out.printls("(C)heating Chelsea");
-		String input1 = Input.nextLine();
-		byte[] choices = new byte["1","2"];
+		System.out.println("(C)heating Chelsea");
+		String input1 = input.nextLine();
+		byte[] choices = new byte["1" "2"];
 		if(input1 == "h") {
-			Scanner Input = new Scanner(System.in);
+			Scanner input = new Scanner(System.in);
 			System.out.println("Choose odd or even (type the number corresponding to your choice)");
 			System.out.println("1- Odds");
 			System.out.println("2- Evens");
-			String input2 = Input.nextLine();
+			String input2 = input.nextLine();
 			
-			Scanner Input = new Scanner(System.in);
+			Scanner input = new Scanner(System.in);
 			System.out.println("1.. 2.. 3... go! (How many fingers - 1 or 2)");
-			String input2 = Input.nextLine();
+			String input2 = input.nextLine();
 			new Random().nextbyte(choices));
 			String generatedString = new String(choices, Charset.forName("UTF-8"));
 			System.out.println(generatedString);
+		}
 			
 		else if(input1 == "c"){
 			Scanner Input = new Scanner(System.in);
-			System.out.println("please choose odds or evens")
-			String input2 = Input.nextLine();
+			System.out.println("please choose odds or evens");
+			String input2 = input.nextLine();
 			if(input2 == "odds"){
-				Scanner Input = new Scanner(System.in);
-				System.out.println("please enter your number")
-				String num_input = Input.nextLine();
+				Scanner input = new Scanner(System.in);
+				System.out.println("please enter your number");
+				String num_input = input.nextLine();
 				if(num_input == "1"){
-					System.out.println("Cheating computer cheats")
-					System.out.println("the computer puts out 1")
-					System.out.println("the total is 2")
-					System.out.println("you lost!")
+					System.out.println("Cheating computer cheats");
+					System.out.println("the computer puts out 1");
+					System.out.println("the total is 2");
+					System.out.println("you lost!");
+				}
 					
 				else if(num_input == "2"){
-					System.out.println("Cheating computer cheats")
-					System.out.println("the computer puts out 2")
-					System.out.println("the total is 4")
-					System.out.println("you lost!")
+					System.out.println("Cheating computer cheats");
+					System.out.println("the computer puts out 2");
+					System.out.println("the total is 4");
+					System.out.println("you lost!");
+				}	
 					
-			else if(input2 == "evens){
-				Scanner Input = new Scanner(System.in);
-				System.out.println("please enter your number")
-				String num_input = Input.nextLine();
+			else if(input2 == "evens"){
+				Scanner input = new Scanner(System.in);
+				System.out.println("please enter your number");
+				String num_input = input.nextLine();
+			}
 				if(num_input == "1"){
-					System.out.println("Cheating computer cheats")
-					System.out.println("the computer puts out 2")
-					System.out.println("the total is 3")
-					System.out.println("you lost!")
+					System.out.println("Cheating computer cheats");
+					System.out.println("the computer puts out 2");
+					System.out.println("the total is 3");
+					System.out.println("you lost!");
+				}
 					
 				else if(num_input == "2"){
-					System.out.println("Cheating computer cheats")
-					System.out.println("the computer puts out 1")
-					System.out.println("the total is 3")
-					System.out.println("you lost!")
-
+					System.out.println("Cheating computer cheats");
+					System.out.println("the computer puts out 1");
+					System.out.println("the total is 3");
+					System.out.println("you lost!"); 
+				}	
 			
 
-	}
-
+			}
+		}
+	};
 }
