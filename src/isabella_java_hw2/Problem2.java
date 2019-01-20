731f4992035a649947cc637971f15950964133c2
MacUser <macuser@MacUsers-Air.lan1>
2019-01-19 22:05:10 -0500

    it works now

--- src/isabella_java_hw2/Problem2.java
+++ src/isabella_java_hw2/Problem2.java
@@ -1,44 +1,22 @@
 package isabella_java_hw2;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.Random;
-		
 
 public class Problem2 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+			
+			String[] available_cards = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Queen", "King", "Jack", "Ace"};  
+			String[] suits = {"♠", "♥", "♦", "♣"};
+			System.out.println("Shuffling the deck!");
+			java.util.Random random = new java.util.Random();
+			int random_computer_card = random.nextInt(available_cards.length);
+			int random_suit = random.nextInt(suits.length);
+			System.out.println("Your card is:");
+			System.out.print("Your card is the " + (random_computer_card) + " of " + random_suit + "s");
 
-				List<String> card_nums = new ArrayList <>();
-				card_nums.add("A"); 
-				card_nums.add("2");
-				card_nums.add("3");
-				card_nums.add("4");
-				card_nums.add("5");
-				card_nums.add("6");
-				card_nums.add("7");
-				card_nums.add("8");
-				card_nums.add("9");
-				card_nums.add("10");
-				card_nums.add("J");
-				card_nums.add("Q");
-				card_nums.add("K");
+	}
+}
 
-				List<String> card_suits = new ArrayList <>();
-				card_suits.add("♠");
-				card_suits.add("♥");
-				card_suits.add("♦");
-				card_suits.add("♣");
-
-				Random rand = new Random();
-				String random_card_num = card_nums.get(rand.nextInt(card_nums.size()));
-				
-				String random_card_suit = card_suits.get(rand.nextInt(card_suits.size()));
-				
-				System.out.print("Your card is the " + (random_card_num) + " of " + random_card_suit + "s");
-				
-				
-			}
-
-		}
