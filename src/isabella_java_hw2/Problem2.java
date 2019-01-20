ba2d18cf55b3a9124589a99cf96edb643e4f296d
Dora83 <43157814+Dora83@users.noreply.github.com>
2019-01-19 22:10:18 -0500

    it works now

--- src/isabella_java_hw2/Problem2.java
+++ src/isabella_java_hw2/Problem2.java
@@ -1,22 +1,21 @@
-// compilation error on line 11
-// please make sure code compiles before submitting
-
-// GRADE: 0/10
-
 package isabella_java_hw2;
-
 import java.util.List;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Random;
 
 public class Problem2 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		String[] available_cards = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Queen", "King", "Jack", "Ace"};  
-		List suits = new List("♠, ♥, ♦, ♣");
-		System.out.println("Shuffling the deck!");
-		java.util.Random random = new java.util.Random();
-		int random_computer_card = random.nextInt(available_cards.length);
-		System.out.println(available_cards[random_computer_card]);
+			
+			String[] available_cards = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Queen", "King", "Jack", "Ace"};  
+			String[] suits = {"♠", "♥", "♦", "♣"};
+			System.out.println("Shuffling the deck!");
+			java.util.Random random = new java.util.Random();
+			int random_computer_card = random.nextInt(available_cards.length);
+			int random_suit = random.nextInt(suits.length);
+			System.out.println("Your card is:");
+			System.out.print("Your card is the " + (random_computer_card) + " of " + random_suit + "s");
+
 	}
-}
+} 
